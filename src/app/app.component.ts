9c095dd8d4e58d2edaf9908b8ec9bdddb11ae6f5
sunilsmail <sunilsmail.amujuri@gmail.com>
2020-10-23 17:08:25 +0530

    changes related to reading data and displaying in UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'GravAngular';
+  title = 'loading';
+  templateString = null;
+  constructor(private http: HttpClient){
+    
+  }
+
+  ngOnInit(){
+    this.http.get('http://localhost/grav-admin/item.json').subscribe((data)=>{
+      this.title = data['frontmatter'].title;
+      this.templateString = data['content']
+      console.log(data)
+    });
+  }
 }
